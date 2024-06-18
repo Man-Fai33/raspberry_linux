7be49eac2412be7609c95c93ceedb29475319c3b
Cheung Man Fai <cheungmanfai330@gmail.com>
2024-06-18 19:00:06 +0800

    RWD

--- react/shopmanagement_system/src/pages/index.tsx
+++ react/shopmanagement_system/src/pages/index.tsx
@@ -76,10 +76,10 @@ export default function Index() {
                 personalProfileDisplay ?
                     <>
                         <div className=" relative h-dvh w-full bg-scroll grid grid-cols-6 gap-4    max-lg:grid-cols-1  max-lg:gap-0   justify-self-center " >
-                            <div className='relative col-span-4 flex p-72  max- max-lg:p-0 bg-slate-500 '
+                            <div className='relative col-span-4 flex p-72   max-xl:p-40 max-lg:p-20 max-md:p-0 bg-slate-500 '
                             // style={{ backgroundImage: `url(${backgroundImage})` }}
                             >
-                                <div className='text-5xl font-mono  content-center w-full space-y-5 max-lg:text-2xl max-lg:text-center  '>
+                                <div className='text-5xl font-mono  content-center w-full space-y-5  max-lg:text-2xl max-lg:text-center  '>
                                     文輝
                                     <br />
                                     Cheung Man Fai
